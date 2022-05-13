f5679d1947eaed6911deb71f158b7c2570f34310
TheoGeneste <tgeneste@alliance-digital.fr>
2022-05-13 17:00:50 +0200

    affichage de tout les lives sur la page d'accueil

--- src/app/videos/videos.module.ts
+++ src/app/videos/videos.module.ts
@@ -4,11 +4,15 @@ import {CommonModule} from '@angular/common';
 import {VideosRoutingModule} from './videos-routing.module';
 import {VideoCardComponent} from './components/video-card/video-card.component';
 import {SharedModule} from '../shared/shared.module';
+import { ClipComponent } from '../clip/clip.component';
+import { VideosComponent } from './containers/videos/videos.component';
 
 
 @NgModule({
   declarations: [
-    VideoCardComponent
+    VideoCardComponent,
+    ClipComponent,
+    VideosComponent
   ],
   exports: [
     VideoCardComponent
